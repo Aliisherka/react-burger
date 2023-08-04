11a3fb3a930c472383c2b815294dc4b4cac4921a
Alisher Issayev <aliisherka@mail.ru>
2023-08-04 23:10:14 +0200

    Вторая версия проекта

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,31 @@
+import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import ModalOverlay from '../ModalOverlay/ModalOverlay';
+import styles from './Modal.module.css';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+
+const modalRoot = document.getElementById("react-modals");
+
+function Modal(props) {
+    return createPortal(
+            (
+                <div className={styles.modal}>
+                    <div className={styles.container}>
+                        <div className={styles.card + ' pl-10 pr-10 pt-10'}>
+                            <h2 className='text text_type_main-large'>{props.title}</h2>
+                            <button type='button' className={styles.close}><CloseIcon type="primary" onClick={props.handleCloseModal}/></button>
+                        </div>
+                        {props.children}
+                    </div>
+                    <ModalOverlay handleCloseModal={props.handleCloseModal}/>
+                </div>
+            ), modalRoot
+        )
+}
+
+Modal.propTypes = {
+    title: PropTypes.string,
+    handleCloseModal: PropTypes.func.isRequired,
+}
+
+export default Modal;
